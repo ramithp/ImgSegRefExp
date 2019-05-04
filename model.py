ebeae24d5d7da14972da82ddfca153f5a65d55ff
Ramith Padaki <ramith.padaki@gmail.com>
2019-05-04 14:46:16 -0400

    Adding more stuff to demo

--- model.py
+++ model.py
@@ -5,9 +5,8 @@ import torch.nn.utils.rnn as rnn_utils
 import torchvision.models as models
 import numpy as np
 
-
 def init_weights(m):
-    if type(m) == nn.Conv2d:
+    if type(m) == nn.Conv2d :
         print("init conv")
         nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
     elif type(m) == nn.BatchNorm2d:
@@ -19,7 +18,7 @@ def init_weights(m):
         torch.nn.init.xavier_normal_(m.weight.data)
     elif type(m) == nn.GRU or type(m) == nn.LSTM:
         for name, param in m.named_parameters():
-            if ('weight' in name):
+            if('weight' in name):
                 print("initializing LSTM/GRU weight ", name)
                 torch.nn.init.xavier_normal_(param)
             elif 'bias' in name:
@@ -28,31 +27,28 @@ def init_weights(m):
 
 
 def conv_relu(kernel_size, stride, in_channels, out_channels, padding=0, bias=True):
-    # TODO: weight init
-    layer = nn.Sequential(
-        nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
-        nn.ReLU(inplace=True))
+    #TODO: weight init
+    layer = nn.Sequential(nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias),
+                      nn.ReLU(inplace=True))
     return layer
-
 
 def conv(kernel_size, stride, in_channels, out_channels, padding=0, bias=True):
-    # TODO: weight init
+    #TODO: weight init
     layer = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, bias=bias)
     return layer
-
-
+    
 def generate_spatial_batch(N, featmap_H, featmap_W):
     spatial_batch_val = np.zeros((N, featmap_H, featmap_W, 8), dtype=np.float32)
     for h in range(featmap_H):
         for w in range(featmap_W):
             xmin = w / featmap_W * 2 - 1
-            xmax = (w + 1) / featmap_W * 2 - 1
-            xctr = (xmin + xmax) / 2
+            xmax = (w+1) / featmap_W * 2 - 1
+            xctr = (xmin+xmax) / 2
             ymin = h / featmap_H * 2 - 1
-            ymax = (h + 1) / featmap_H * 2 - 1
-            yctr = (ymin + ymax) / 2
+            ymax = (h+1) / featmap_H * 2 - 1
+            yctr = (ymin+ymax) / 2
             spatial_batch_val[:, h, w, :] = \
-                [xmin, ymin, xmax, ymax, xctr, yctr, 1 / featmap_W, 1 / featmap_H]
+                [xmin, ymin, xmax, ymax, xctr, yctr, 1/featmap_W, 1/featmap_H]
     return torch.Tensor(spatial_batch_val)
 
 
@@ -61,14 +57,14 @@ class LanguageModule(nn.Module):
         super(LanguageModule, self).__init__()
 
         # TODO: padding index
-        self.embedding = nn.Embedding(vocab_size, emb_size)  # , padding_idx=??)
+        self.embedding = nn.Embedding(vocab_size, emb_size) #, padding_idx=??)
 
         # TODO init bias as 1: https://github.com/ramithp/text_objseg/blob/tensorflow-1.x-compatibility/models/lstm_net.py#L16
         # Not a bi-LSTM: https://github.com/ramithp/text_objseg/blob/tensorflow-1.x-compatibility/util/rnn.py#L57
         self.lstm = nn.LSTM(input_size=emb_size,
-                            hidden_size=hidden_size,
-                            num_layers=num_lstm_layers,
-                            bidirectional=False, batch_first=True)
+                        hidden_size=hidden_size,
+                        num_layers=num_lstm_layers,
+                        bidirectional=False, batch_first=True)
 
     def forward(self, input_seq):
         # Incoming is a bsz x seq_len
@@ -82,19 +78,19 @@ class LanguageModule(nn.Module):
 
         # Retrieve embeddings it into bsz x seq_len x embedding_size; including for <pad> tokens
         embedded_seqs = self.embedding(input_seq)
-
+        
         # Goes in as bsz x seq_len x embedding_dim
         # Comes out of this as bsz x seq_len x embedding_dim
         packed_seqs = rnn_utils.pack_padded_sequence(embedded_seqs, lengths=in_lens, batch_first=True)
 
-        hidden = None  # internally sets to 0 vector embeddings
+        hidden = None # internally sets to 0 vector embeddings
         output_packed, (hidden, _) = self.lstm(packed_seqs, hidden)
-
+        
         # Output is now seq_len x bsz x hidden_dim
         return hidden[-1]
 
-
 class ImageModule(nn.Module):
+
 
     def __init__(self):
         super(ImageModule, self).__init__()
@@ -102,11 +98,10 @@ class ImageModule(nn.Module):
         # Freeze VGG weights for all layers except FC layers
         self.feature_extractor = models.vgg16(pretrained=True)
         self.feature_extractor = self.feature_extractor.features
-
+        
         # Padding is 3x3 because after VGG16 layers, we get a 16x16 feature map. k=7 needs 5 to get "SAME" padding
-        vgg_fc7_full_conv = nn.Sequential(
-            conv_relu(kernel_size=7, stride=1, in_channels=512, out_channels=4096, padding=(3, 3)),
-            conv_relu(kernel_size=1, stride=1, in_channels=4096, out_channels=4096))
+        vgg_fc7_full_conv = nn.Sequential(conv_relu(kernel_size=7, stride=1, in_channels=512, out_channels=4096, padding=(3, 3)),
+                                  conv_relu(kernel_size=1, stride=1, in_channels=4096, out_channels=4096))
 
         # Padding not needed. Just a 1x1
         vgg_fc8_full_conv = conv(kernel_size=1, stride=1, in_channels=4096, out_channels=1000)
@@ -115,57 +110,28 @@ class ImageModule(nn.Module):
         self.feature_extractor.add_module("vgg_fc8_full_conv", vgg_fc8_full_conv)
 
     def forward(self, inputs):
-        x = self.feature_extractor(inputs)
-        print(x.shape)
-        return x
-
-
-class ImageModuleResnet(nn.Module):
-
-    def __init__(self):
-        super(ImageModuleResnet, self).__init__()
-
-        # Freeze VGG weights for all layers except FC layers
-        self.feature_extractor = models.resnet101(pretrained=True)
-        self.feature_extractor = torch.nn.Sequential(*list(self.feature_extractor.children())[:-2])
-
-        # Padding is 3x3 because after VGG16 layers, we get a 16x16 feature map. k=7 needs 5 to get "SAME" padding
-        res_fc7_full_conv = nn.Sequential(
-            conv_relu(kernel_size=7, stride=1, in_channels=2048, out_channels=4096, padding=(3, 3)),
-            conv_relu(kernel_size=1, stride=1, in_channels=4096, out_channels=4096))
-
-        # Padding not needed. Just a 1x1
-        res_fc8_full_conv = conv(kernel_size=1, stride=1, in_channels=4096, out_channels=1000)
-
-        self.feature_extractor.add_module("resnet_fc7_full_conv", res_fc7_full_conv)
-        self.feature_extractor.add_module("resnet_fc8_full_conv", res_fc8_full_conv)
-
-    def forward(self, inputs):
-        x = self.feature_extractor(inputs)
+        x  = self.feature_extractor(inputs)
         print(x.shape)
         return x
 
 
 class DeconvLayer(nn.Module):
-    def __init__(self, kernel_size, stride, output_dim, bias=False, in_channels=1):
+    def __init__(self, kernel_size, stride, output_dim, bias=False):
         super(DeconvLayer, self).__init__()
-        self.dconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=output_dim, kernel_size=kernel_size,
+        self.dconv = nn.ConvTranspose2d(in_channels=1, out_channels=output_dim, kernel_size=kernel_size, 
                                         stride=stride, bias=bias, padding=16)
-
+        
     def forward(self, inp):
         # batch_size, input_dim, input_height, input_width = inp.shape
         return self.dconv(inp)
-
 
 class ImgSegRefExpModel(nn.Module):
     """
     Our implementation of https://arxiv.org/pdf/1603.06180.pdf
     """
-
     def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
         super(ImgSegRefExpModel, self).__init__()
-        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1,
-                                            hidden_size=lstm_hidden_size)
+        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1, hidden_size=lstm_hidden_size)
 
         self.img_features = ImageModule()
 
@@ -188,34 +154,33 @@ class ImgSegRefExpModel(nn.Module):
         N, D_text = text_out.size(0), text_out.size(1)
 
         # Tile the textual features with the image feature-maps
-        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)
+        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)        
 
         # Generate spatial features to learn co-ordinates
         spatial_feats = generate_spatial_batch(N, featmap_H, featmap_W).permute(0, 3, 1, 2)
-
+        
         # Concat 3 sources of inputs
         # Output is of shape N x (D_text + D_img + D_spatial) x H x W
         concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
-                                F.normalize(img_out, p=2, dim=1),
-                                spatial_feats], dim=1)
+                             F.normalize(img_out, p=2, dim=1),
+                             spatial_feats], dim=1)
 
         # Series of linear layers to reduce dimensions
         mlp_out = self.mlp1(concat_out)
         mlp_out = self.mlp2(mlp_out)
-
+        
         # Final deconvolution to get the upsampled mask
         generated_mask = self.deconv(mlp_out)
 
-        return generated_mask
+        return generated_mask                
 
 
-class ResImgSeg(nn.Module):
+class ContextualImgSegRefExpModel(nn.Module):
     def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
-        super(ResImgSeg, self).__init__()
-        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1,
-                                            hidden_size=lstm_hidden_size)
+        super(ImgSegRefExpModel, self).__init__()
+        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1, hidden_size=lstm_hidden_size)
 
-        self.img_features = ImageModuleResnet()
+        self.img_features = ImageModule()
 
         self.mlp1 = conv_relu(kernel_size=1, stride=1, in_channels=1000 + lstm_hidden_size + 8, out_channels=mlp_hidden)
         self.mlp2 = nn.Sequential(conv(kernel_size=1, stride=1, in_channels=mlp_hidden, out_channels=1))
@@ -236,72 +201,22 @@ class ResImgSeg(nn.Module):
         N, D_text = text_out.size(0), text_out.size(1)
 
         # Tile the textual features with the image feature-maps
-        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)
+        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)        
 
         # Generate spatial features to learn co-ordinates
         spatial_feats = generate_spatial_batch(N, featmap_H, featmap_W).permute(0, 3, 1, 2)
-
+        
         # Concat 3 sources of inputs
         # Output is of shape N x (D_text + D_img + D_spatial) x H x W
         concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
-                                F.normalize(img_out, p=2, dim=1),
-                                spatial_feats], dim=1)
+                             F.normalize(img_out, p=2, dim=1),
+                             spatial_feats], dim=1)
 
         # Series of linear layers to reduce dimensions
         mlp_out = self.mlp1(concat_out)
         mlp_out = self.mlp2(mlp_out)
-
+        
         # Final deconvolution to get the upsampled mask
         generated_mask = self.deconv(mlp_out)
 
-        return generated_mask
-
-
-class ResImgSegDeconved(nn.Module):
-    def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
-        super(ResImgSegDeconved, self).__init__()
-        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1,
-                                            hidden_size=lstm_hidden_size)
-
-        self.img_features = ImageModuleResnet()
-
-        self.mlp1 = conv_relu(kernel_size=1, stride=1, in_channels=1000 + lstm_hidden_size + 8, out_channels=256)
-
-        # https://pytorch.org/docs/stable/nn.html#convtranspose2d
-        self.deconv = DeconvLayer(kernel_size=64, stride=32, output_dim=mlp_hidden, bias=False, in_channels=256)
-
-        self.mlp2 = nn.Sequential(conv(kernel_size=1, stride=1, in_channels=mlp_hidden, out_channels=1))
-
-    def forward(self, inputs):
-        img_input, text_input = inputs
-
-        img_out = self.img_features(img_input)
-        text_out = self.text_features(text_input)
-
-        # N C H W format
-        featmap_H, featmap_W = img_out.size(2), img_out.size(3)
-        print(featmap_H, featmap_W)
-        # bsz x hidden_dim
-        N, D_text = text_out.size(0), text_out.size(1)
-
-        # Tile the textual features with the image feature-maps
-        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)
-
-        # Generate spatial features to learn co-ordinates
-        spatial_feats = generate_spatial_batch(N, featmap_H, featmap_W).permute(0, 3, 1, 2)
-
-        # Concat 3 sources of inputs
-        # Output is of shape N x (D_text + D_img + D_spatial) x H x W
-        concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
-                                F.normalize(img_out, p=2, dim=1),
-                                spatial_feats], dim=1)
-
-        # Series of linear layers to reduce dimensions
-        mlp_out = self.mlp1(concat_out)
-
-        # Final deconvolution to get the upsampled mask
-        deconv_out = self.deconv(mlp_out)
-
-        generated_mask = self.mlp2(deconv_out)
-
-        return F.softmax(generated_mask)
+        return generated_mask                
