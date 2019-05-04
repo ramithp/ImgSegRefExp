b3f9fdc5b07a75297eefede4e5389ede0558ea0f
Nikhil Bhale <nikhilbhale@gmail.com>
2019-05-03 23:34:58 -0400

    Adding Resnet plus deconv ImgSegRefExp model

--- model.py
+++ model.py
@@ -147,9 +147,9 @@ class ImageModuleResnet(nn.Module):
 
 
 class DeconvLayer(nn.Module):
-    def __init__(self, kernel_size, stride, output_dim, bias=False):
+    def __init__(self, kernel_size, stride, output_dim, bias=False, in_channels=1):
         super(DeconvLayer, self).__init__()
-        self.dconv = nn.ConvTranspose2d(in_channels=1, out_channels=output_dim, kernel_size=kernel_size,
+        self.dconv = nn.ConvTranspose2d(in_channels=in_channels, out_channels=output_dim, kernel_size=kernel_size,
                                         stride=stride, bias=bias, padding=16)
 
     def forward(self, inp):
@@ -209,9 +209,9 @@ class ImgSegRefExpModel(nn.Module):
         return generated_mask
 
 
-class ContextualImgSegRefExpModel(nn.Module):
+class ResImgSeg(nn.Module):
     def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
-        super(ContextualImgSegRefExpModel, self).__init__()
+        super(ResImgSeg, self).__init__()
         self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1,
                                             hidden_size=lstm_hidden_size)
 
@@ -255,3 +255,53 @@ class ContextualImgSegRefExpModel(nn.Module):
         generated_mask = self.deconv(mlp_out)
 
         return generated_mask
+
+
+class ResImgSegDeconved(nn.Module):
+    def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
+        super(ResImgSegDeconved, self).__init__()
+        self.text_features = LanguageModule(vocab_size=vocab_size, emb_size=emb_size, num_lstm_layers=1,
+                                            hidden_size=lstm_hidden_size)
+
+        self.img_features = ImageModuleResnet()
+
+        self.mlp1 = conv_relu(kernel_size=1, stride=1, in_channels=1000 + lstm_hidden_size + 8, out_channels=256)
+
+        # https://pytorch.org/docs/stable/nn.html#convtranspose2d
+        self.deconv = DeconvLayer(kernel_size=64, stride=32, output_dim=mlp_hidden, bias=False, in_channels=256)
+
+        self.mlp2 = nn.Sequential(conv(kernel_size=1, stride=1, in_channels=mlp_hidden, out_channels=1))
+
+    def forward(self, inputs):
+        img_input, text_input = inputs
+
+        img_out = self.img_features(img_input)
+        text_out = self.text_features(text_input)
+
+        # N C H W format
+        featmap_H, featmap_W = img_out.size(2), img_out.size(3)
+        print(featmap_H, featmap_W)
+        # bsz x hidden_dim
+        N, D_text = text_out.size(0), text_out.size(1)
+
+        # Tile the textual features with the image feature-maps
+        text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)
+
+        # Generate spatial features to learn co-ordinates
+        spatial_feats = generate_spatial_batch(N, featmap_H, featmap_W).permute(0, 3, 1, 2)
+
+        # Concat 3 sources of inputs
+        # Output is of shape N x (D_text + D_img + D_spatial) x H x W
+        concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
+                                F.normalize(img_out, p=2, dim=1),
+                                spatial_feats], dim=1)
+
+        # Series of linear layers to reduce dimensions
+        mlp_out = self.mlp1(concat_out)
+
+        # Final deconvolution to get the upsampled mask
+        deconv_out = self.deconv(mlp_out)
+
+        generated_mask = self.mlp2(deconv_out)
+
+        return F.softmax(generated_mask)
