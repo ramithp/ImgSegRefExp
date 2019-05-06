6e7083af3f0fda0034af13207e4c28938ae366c3
Nishad94 <nishad.dawkhar94@gmail.com>
2019-05-06 17:12:44 -0400

    Updated visual context with new attn code

--- model/res_attn_visual_updated.py
+++ model/res_attn_visual_updated.py
@@ -10,6 +10,7 @@ from model.model_utils import generate_spatial_batch, conv_relu, conv, init_weig
 import config
 from baseline import VisualContextModule
 
+
 class ResAttnImgSeg(nn.Module):
     def __init__(self, mlp_hidden, vocab_size, emb_size, lstm_hidden_size):
         super(ResAttnImgSeg, self).__init__()
@@ -17,13 +18,11 @@ class ResAttnImgSeg(nn.Module):
                                             hidden_size=lstm_hidden_size, return_all=True)
 
         self.img_features = ImageModuleResnet(resnet_weights_file=config.resnet_wts_file)
-        img_feats_channels = 1000
+		img_feats_channels = 1000
         Cg = 256
-        
-        self.mlp1 = conv_relu(kernel_size=1, stride=1, in_channels=img_feats_channels + lstm_hidden_size + 8 + Cg, out_channels=mlp_hidden)
+
+        self.mlp1 = conv_relu(kernel_size=1, stride=1, in_channels=1000 + lstm_hidden_size + 8 + Cg, out_channels=mlp_hidden)
         self.mlp2 = nn.Sequential(conv(kernel_size=1, stride=1, in_channels=mlp_hidden, out_channels=1))
-        
-        self.visual_context = VisualContextModule(Cg,img_feats_channels + 8)
 
         # init
         self.mlp1.apply(init_weights)
@@ -43,7 +42,7 @@ class ResAttnImgSeg(nn.Module):
         max_inp_len = torch.max(input_lens).item()
         #         pdb.set_trace()
         out_mod = torch.arange(max_inp_len).cuda().unsqueeze(0).repeat([batch_size, 1])
-        out = out_mod < (max_inp_len - input_lens.unsqueeze(1))
+        out = out_mod >= input_lens.unsqueeze(1)
         return out.byte()
 
     def get_attn_weights(self, keys, queries):
@@ -62,7 +61,7 @@ class ResAttnImgSeg(nn.Module):
         alphas.masked_fill_(mask.unsqueeze(1).unsqueeze(1), -float("inf"))  # fills -inf where mask is 1
         weights_over_text = F.softmax(alphas, dim=-1)  # B x H x W x T
         att_vecs = torch.matmul(weights_over_text, lstm_output.unsqueeze(1))  # B x H x W x hdim
-        return att_vecs,weights_over_text
+        return att_vecs, weights_over_text
 
     def forward(self, inputs):
         img_input, text_input = inputs
@@ -80,21 +79,18 @@ class ResAttnImgSeg(nn.Module):
         # Tile the textual features with the image feature-maps
         # text_out = text_out.unsqueeze(-1).unsqueeze(-1).repeat(1, 1, featmap_H, featmap_W)
 
-        text_out,weights_over_text = self.get_attn_vec(text_out, text_lens, img_out.permute(0, 2, 3, 1))
+		text_out,weights_over_text = self.get_attn_vec(text_out, text_lens, img_out.permute(0, 2, 3, 1))
         #         pdb.set_trace()
         text_out = text_out.permute(0, 3, 1, 2)
 
         # Generate spatial features to learn co-ordinates
         spatial_feats = generate_spatial_batch(N, featmap_H, featmap_W).permute(0, 3, 1, 2)
-        visual_context = self.visual_context.forward(torch.cat([F.normalize(img_out, p=2, dim=1),
+		visual_context = self.visual_context.forward(torch.cat([F.normalize(img_out, p=2, dim=1),
                                 spatial_feats], dim=1),weights_over_text.permute(0,3,1,2))
-
-        # Generate spatial features to learn co-ordinates
-        
         #         pdb.set_trace()
         # Concat 3 sources of inputs
-        # Output is of shape N x (D_text + D_img + D_spatial + Cg) x H x W
-        concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
+        # Output is of shape N x (D_text + D_img + D_spatial) x H x W
+		concat_out = torch.cat([F.normalize(text_out, p=2, dim=1),
                                 F.normalize(img_out, p=2, dim=1),
                                 spatial_feats,visual_context], dim=1)
 
